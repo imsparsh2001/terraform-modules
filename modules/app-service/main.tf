e64b218a8b1bfc0d2cf60c83a736ad03789f9918
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-04 16:08:03 +0530

    Update main.tf

--- modules/app-service/main.tf
+++ modules/app-service/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_service_plan" "app_service_plan" { # Updated resource name
   name                = var.app_service_plan
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -11,13 +11,35 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   }
 }
 
-resource "azurerm_linux_web_app" "app_service" {  # Updated resource
+resource "azurerm_linux_web_app" "app_service" {
   name                = var.app_service_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  service_plan_id     = azurerm_app_service_plan.app_service_plan.id
+  service_plan_id     = azurerm_service_plan.app_service_plan.id # Updated to reference azurerm_service_plan
+
+  linux_fx_version    = "DOCKER|nginx:latest" # Moved out of site_config
 
   site_config {
-    linux_fx_version = "DOCKER|nginx:latest"
+   
   }
+}
+
+variable "app_service_plan" {
+  description = "App service plan name"
+  type        = string
+}
+
+variable "app_service_name" {
+  description = "App service name"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resource group"
+  type        = string
 }
