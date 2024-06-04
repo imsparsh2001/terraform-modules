cb4e43860a66fed7147ce2519905e00f2c22aed0
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-04 16:18:43 +0530

    Update main.tf

--- modules/app-service/main.tf
+++ modules/app-service/main.tf
@@ -1,21 +1,26 @@
 resource "azurerm_service_plan" "app_service_plan" {
-  name                = var.app_service_plan
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  os_type             = "Linux"                      # Required argument
-  sku_name            = "B1"                         # Required argument
+  name                   = var.app_service_plan_name  # Use a descriptive name
+  location               = var.location
+  resource_group_name    = var.resource_group_name
 
-  # other configurations if necessary
+  # Required arguments
+  kind                   = "Linux"
+  sku {
+    tier  = "Basic"
+    size  = "B1"
+  }
+
+  # Other configurations if necessary
 }
 
-resource "azurerm_linux_web_app" "app_service" {
-  name                = var.app_service_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  service_plan_id     = azurerm_service_plan.app_service_plan.id
+resource "azurerm_app_service" "app_service" {
+  name                   = var.app_service_name
+  location               = var.location
+  resource_group_name    = var.resource_group_name
+  app_service_plan_id     = azurerm_service_plan.app_service_plan.id
 
   site_config {
-    linux_fx_version    = "DOCKER|nginx:latest"        # Correct placement of linux_fx_version
-    # other configurations if necessary
+    linux_fx_version = "DOCKER|nginx:latest"  # Correct placement
+    # Other configurations if necessary
   }
 }
