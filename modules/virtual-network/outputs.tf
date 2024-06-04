7c9cbb2a24ef32346a358ae00374bccd538136fc
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-04 14:42:11 +0530

    Update outputs.tf

--- modules/virtual-network/outputs.tf
+++ modules/virtual-network/outputs.tf
@@ -1,12 +1,7 @@
-
-output "resource_group_name" {
-  value = module.resource_group.resource_group_name
-}
-
 output "vnet_id" {
-  value = module.virtual_network.vnet_id
+  value = azurerm_virtual_network.vnet.id
 }
 
 output "subnet_id" {
-  value = module.virtual_network.subnet_id
+  value = azurerm_subnet.subnet.id
 }
