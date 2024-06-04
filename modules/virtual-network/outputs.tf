04522d4bf23e8fb8684371e5f59354bd8fe6dbd1
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-04 14:25:58 +0530

    Update outputs.tf

--- modules/virtual-network/outputs.tf
+++ modules/virtual-network/outputs.tf
@@ -1,1 +1,12 @@
 
+output "resource_group_name" {
+  value = module.resource_group.resource_group_name
+}
+
+output "vnet_id" {
+  value = module.virtual_network.vnet_id
+}
+
+output "subnet_id" {
+  value = module.virtual_network.subnet_id
+}
