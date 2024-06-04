575c678703d08f3f2693769a71161a3d15395716
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-04 16:08:37 +0530

    Update main.tf

--- modules/app-service/main.tf
+++ modules/app-service/main.tf
@@ -20,26 +20,6 @@ resource "azurerm_linux_web_app" "app_service" {
   linux_fx_version    = "DOCKER|nginx:latest" # Moved out of site_config
 
   site_config {
-   
+    # other configurations if necessary
   }
-}
-
-variable "app_service_plan" {
-  description = "App service plan name"
-  type        = string
-}
-
-variable "app_service_name" {
-  description = "App service name"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "The location of the resource group"
-  type        = string
 }
