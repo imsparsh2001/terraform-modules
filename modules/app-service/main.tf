ff70630178b539ffb109d2e98b9cbcf38ce732c4
imsparsh2001 <sparshjaipuriyar@gmail.com>
2024-06-04 15:57:08 +0530

    created app services

--- modules/app-service/main.tf
+++ modules/app-service/main.tf
@@ -11,11 +11,11 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   }
 }
 
-resource "azurerm_app_service" "app_service" {
+resource "azurerm_linux_web_app" "app_service" {  # Updated resource
   name                = var.app_service_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  service_plan_id     = azurerm_app_service_plan.app_service_plan.id
 
   site_config {
     linux_fx_version = "DOCKER|nginx:latest"
