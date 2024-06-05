6a74569862c3824da26435b1b8e9463e21e72f54
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-06 04:04:58 +0530

    Update outputs.tf

--- modules/app-service/outputs.tf
+++ modules/app-service/outputs.tf
@@ -1,4 +1,4 @@
 output "web_app_hostname" {
-  value       = azurerm_linux_web_app.example.default_hostname
+  value       = azurerm_linux_web_app.example.default_hostname  
   description = "The default hostname of the Linux Web App"
 }
