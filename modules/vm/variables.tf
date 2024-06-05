1ae28fbfad78c2120f41290091a71f8fb3a2196b
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-06 02:38:03 +0530

    Update variables.tf

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -20,28 +20,4 @@ variable "vm_configs" {
     admin_username = string
     admin_password = string
   }))
-  default = {
-    "vm1" = {
-      vm_name        = "vm1",
-      vm_size        = "Standard_DS1_v2",
-      subnet_id      = "subnet_id_here",  # Replace with actual subnet ID
-      os_publisher   = "Canonical",
-      os_offer       = "UbuntuServer",
-      os_sku         = "18.04-LTS",
-      os_disk_size   = 30,
-      admin_username = "azureuser",
-      admin_password = "ComplexPassword1234!"
-    },
-    "vm2" = {
-      vm_name        = "vm2",
-      vm_size        = "Standard_DS2_v2",
-      subnet_id      = "subnet_id_here",  # Replace with actual subnet ID
-      os_publisher   = "Canonical",
-      os_offer       = "UbuntuServer",
-      os_sku         = "20.04-LTS",
-      os_disk_size   = 30,
-      admin_username = "azureuser",
-      admin_password = "AnotherComplexPassword1234!"
-    }
-  }
 }
