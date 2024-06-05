2af576986249bbd8eaf2776010111aba13fff21c
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-06 05:02:17 +0530

    Update main.tf

--- modules/app-service/main.tf
+++ modules/app-service/main.tf
@@ -1,17 +1,16 @@
 resource "azurerm_service_plan" "example" {
   name                = "${var.name}-service-plan"
-  location            = var.location
   resource_group_name = var.resource_group_name
+  location            = var.location
   os_type             = "Linux"
   sku_name            = var.sku_name
 }
 
 resource "azurerm_linux_web_app" "example" {
   name                = "${var.name}-web-app"
-  location            = var.location
   resource_group_name = var.resource_group_name
+  location            = var.location
   service_plan_id     = azurerm_service_plan.example.id
-  linux_fx_version    = var.linux_fx_version 
 
   site_config {
     always_on = true
