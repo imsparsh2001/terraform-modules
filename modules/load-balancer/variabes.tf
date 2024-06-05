d79f6ce3b5c9ec19400060b281592b17564b7e1e
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-05 05:46:59 +0530

    Update variabes.tf

--- modules/load-balancer/variabes.tf
+++ modules/load-balancer/variabes.tf
@@ -5,3 +5,19 @@ variable "resource_group_name" {
 variable "location" {
   type = string
 }
+
+variable "frontend_ip_name" {
+  type = string
+}
+
+variable "backend_pool_name" {
+  type = string
+}
+
+variable "lb_name" {
+  type = string
+}
+
+variable "vnet_subnet_id" {
+  type = string
+}
