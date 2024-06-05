f43c09d72196e1005ec74dc97675663776cf23ae
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-06 02:25:29 +0530

    Update outputs.tf

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,15 +1,17 @@
-output "linux_vm_id" {
-  value = azurerm_virtual_machine.linux_vm.id
+output "vm_ids" {
+  value = {for key, vm in azurerm_virtual_machine.vm : key => vm.id}
 }
 
-output "windows_vm_id" {
-  value = azurerm_virtual_machine.windows_vm.id
+output "vm_names" {
+  value = { for key, vm in azurerm_virtual_machine.vm : key => vm.name }
+  description = "The names of the virtual machines."
 }
 
-output "linux_vm_private_ip" {
-  value = azurerm_network_interface.linux_nic.private_ip_address
+output "nic_ids" {
+  value = {for key, nic in azurerm_network_interface.vm_nic : key => nic.id}
 }
 
-output "windows_vm_private_ip" {
-  value = azurerm_network_interface.windows_nic.private_ip_address
+output "public_ip_addresses" {
+  value = { for key, nic in azurerm_network_interface.vm_nic : key => nic.ip_configuration.0.public_ip_address_id }
+  description = "Public IP addresses of the VMs, if applicable."
 }
