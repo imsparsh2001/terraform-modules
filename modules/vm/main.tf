f1ed5356557b6c2c3469821f2551981618ed05d0
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-06 02:22:52 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,89 +1,54 @@
-# Linux VM
-resource "azurerm_virtual_machine" "linux_vm" {
-  name                  = "LinuxVM"
+resource "azurerm_virtual_machine" "vm" {
+  for_each = var.vm_configs
+
+  name                  = each.value.vm_name
   location              = var.location
   resource_group_name   = var.resource_group_name
-  vm_size               = "Standard_DS1_v2"
-  network_interface_ids = [azurerm_network_interface.linux_nic.id]
+  network_interface_ids = [azurerm_network_interface.vm_nic[each.key].id]
+  vm_size               = each.value.vm_size
 
+  # OS Profile
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    publisher = each.value.os_publisher
+    offer     = each.value.os_offer
+    sku       = each.value.os_sku
     version   = "latest"
   }
 
+  storage_os_disk {
+    name              = "${each.value.vm_name}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+    disk_size_gb      = each.value.os_disk_size
+  }
+
   os_profile {
-    computer_name  = "LinuxVM"
-    admin_username = "adminuser"
-    admin_password = "Password123!"
+    computer_name  = each.value.vm_name
+    admin_username = each.value.admin_username
+    admin_password = each.value.admin_password
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
 
-  storage_os_disk {
-    name              = "myLinuxOSDisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+  tags = {
+    environment = "production"
   }
 }
 
-resource "azurerm_network_interface" "linux_nic" {
-  name                = "LinuxNIC"
+resource "azurerm_network_interface" "vm_nic" {
+  for_each = var.vm_configs
+
+  name                = "${each.value.vm_name}-nic"
   location            = var.location
   resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = var.subnet_id
+    subnet_id                     = each.value.subnet_id
     private_ip_address_allocation = "Dynamic"
-  }
-}
-
-# Windows VM
-resource "azurerm_virtual_machine" "windows_vm" {
-  name                  = "WindowsVM"
-  location              = var.location
-  resource_group_name   = var.resource_group_name  # Corrected argument name
-  vm_size               = "Standard_DS1_v2"
-  network_interface_ids = [azurerm_network_interface.windows_nic.id]
-
-  storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-
-  os_profile {
-    computer_name  = "WindowsVM"
-    admin_username = "adminuser"
-    admin_password = "Password123!"
-  }
-
-  os_profile_windows_config {
-    provision_vm_agent = true
-  }
-
-  storage_os_disk {
-    name              = "myWindowsOSDisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-}
-
-resource "azurerm_network_interface" "windows_nic" {
-  name                = "WindowsNIC"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
+   
   }
 }
