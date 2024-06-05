d0e734f13530110eb8b9bb5b058996da33b02431
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-05 05:47:59 +0530

    Update main.tf

--- modules/load-balancer/main.tf
+++ modules/load-balancer/main.tf
@@ -1,46 +1,43 @@
-
-
-resource "azurerm_public_ip" "lb_public_ip" {
-  name                = "lbPublicIP"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
+# Load Balancer
 resource "azurerm_lb" "main" {
-  name                = "myLoadBalancer"
+  name                = var.lb_name
   location            = var.location
   resource_group_name = var.resource_group_name
 
   frontend_ip_configuration {
-    name                 = "publicIPAddress"
-    public_ip_address_id = azurerm_public_ip.lb_public_ip.id
+    name                 = var.frontend_ip_name
+    public_ip_address_id = azurerm_public_ip.lb.id
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "backend_pool" {
-  loadbalancer_id = azurerm_lb.main.id
-  name            = "myBackendPool"
+resource "azurerm_public_ip" "lb" {
+  name                = "${var.lb_name}-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
 }
 
-resource "azurerm_lb_probe" "health_probe" {
+resource "azurerm_lb_backend_address_pool" "backend" {
+  loadbalancer_id = azurerm_lb.main.id
+  name            = var.backend_pool_name
+}
+
+resource "azurerm_lb_probe" "probe" {
   resource_group_name = var.resource_group_name
   loadbalancer_id     = azurerm_lb.main.id
-  name                = "http-probe"
-  protocol            = "Http"
-  request_path        = "/healthcheck"
+  name                = "health-probe"
+  protocol            = "Tcp"
   port                = 80
 }
 
-resource "azurerm_lb_rule" "lb_rule" {
+resource "azurerm_lb_rule" "rule" {
   resource_group_name            = var.resource_group_name
   loadbalancer_id                = azurerm_lb.main.id
-  name                           = "httpRule"
+  name                           = "lb-rule"
   protocol                       = "Tcp"
   frontend_port                  = 80
   backend_port                   = 80
-  frontend_ip_configuration_name = "publicIPAddress"
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.backend_pool.id
-  probe_id                       = azurerm_lb_probe.health_probe.id
+  frontend_ip_configuration_name = var.frontend_ip_name
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.backend.id
+  probe_id                       = azurerm_lb_probe.probe.id
 }
