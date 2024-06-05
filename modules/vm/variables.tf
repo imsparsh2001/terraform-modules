5790313370413338a615ae5119859925251c9879
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-06 02:30:44 +0530

    Update variables.tf

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,3 +1,12 @@
+variable "location" {
+  description = "The Azure location where the resources will be created"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
 variable "vm_configs" {
   description = "Configuration for each VM"
   type = map(object({
