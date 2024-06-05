af9498d32c4deafeb6b5556d6b0df576afa8ce16
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-06 04:20:56 +0530

    Update main.tf

--- modules/app-service/main.tf
+++ modules/app-service/main.tf
@@ -12,5 +12,11 @@ resource "azurerm_linux_web_app" "example" {
   resource_group_name = var.resource_group_name
   service_plan_id     = azurerm_service_plan.example.id
 
+  site_config {
+    # Basic site_config block to satisfy the provider requirements
+    linux_fx_version = var.linux_fx_version
+    always_on        = true
+  }
+
   app_settings = var.app_settings
 }
