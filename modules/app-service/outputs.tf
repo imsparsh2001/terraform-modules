15bab2048f7f9af2877aee0db2418bd5f19c65fc
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2024-06-06 05:03:21 +0530

    Update outputs.tf

--- modules/app-service/outputs.tf
+++ modules/app-service/outputs.tf
@@ -1,9 +1,11 @@
 output "service_plan_id" {
-  value       = azurerm_service_plan.example.id
-  description = "The ID of the App Service Plan"
+  value = azurerm_service_plan.example.id
 }
 
-output "web_app_hostname" {
-  value       = azurerm_linux_web_app.example.default_hostname
-  description = "The default hostname of the Linux Web App"
+output "web_app_id" {
+  value = azurerm_linux_web_app.example.id
+}
+
+output "web_app_default_hostname" {
+  value = azurerm_linux_web_app.example.default_hostname
 }
