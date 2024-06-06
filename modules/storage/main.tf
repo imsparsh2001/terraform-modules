8b192f3d12da4ed8fbb3a71e26233ef4a0476f2d
imsparsh2001 <sparshjaipuriyar@gmail.com>
2024-06-06 10:30:25 +0530

    created storage account

---
+++ modules/storage/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_storage_account" "tfstate" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
